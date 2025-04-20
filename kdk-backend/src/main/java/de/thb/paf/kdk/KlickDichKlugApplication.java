d155f53687002a62a2e7d54f7fec9d47bf47cbe3
BettyRe <elisabeth.reckel@outlook.de>
2025-04-20 14:44:51 +0200

    added spring boot starter files

---
+++ kdk-backend/src/main/java/de/thb/paf/kdk/KlickDichKlugApplication.java
@@ -0,0 +1,13 @@
+package de.thb.paf.kdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlickDichKlugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlickDichKlugApplication.class, args);
+	}
+
+}
---
+++ kdk-backend/src/test/java/de/thb/paf/kdk/KlickDichKlugApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thb.paf.kdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlickDichKlugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
